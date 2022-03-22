d47df7906cd42b95b6418102af340785c103703c
YumanYummy <ahmadrizki0209@gmail.com>
2022-03-22 10:24:00 +0700

    Reupload Source

---
+++ Tes/src/tes/ye.java
@@ -0,0 +1,6 @@
+public class ye{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Bujank");
+    }
+
+}    
---
+++ Tes/src/tesss.java
@@ -0,0 +1,5 @@
+public class tesss {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Bujank");
+    }
+}
