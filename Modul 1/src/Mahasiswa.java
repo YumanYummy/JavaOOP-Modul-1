d24a0121a9aae1ef38e343b33ae8501e12af032b
YumanYummy <ahmadrizki0209@gmail.com>
2022-03-28 00:05:14 +0700

    Nothing Diffrent

--- Modul 1/src/Mahasiswa.java
+++ Modul 1/src/Mahasiswa.java
@@ -1,78 +1,54 @@
 import java.util.Scanner;
 
 public class Mahasiswa {
-    Scanner in = new Scanner(System.in);
-
-    private String Mahasiswanama;
-    private String Mahasiswanim;
-    private String Password;
-    
-    public void setMahasiswanama(String Mahasiswanama){
-        this.Mahasiswanama = Mahasiswanama;
-    }
-    public String getMahasiswanama(){
-        return Mahasiswanama;
-    }
-    
-    public void setMahasiswanim(String Mahasiswanim){
-        if(Mahasiswanim.length() != 15){
-            System.out.println("Nim Harus 15 Angka");
-            System.out.print("Nim : ");
-            Mahasiswanim = in.next();
-            setMahasiswanim(Mahasiswanim);
-        }else{
-            this.Mahasiswanim = Mahasiswanim;
-        }
-    }
-    public String getMahasiswanim(){
-        return Mahasiswanim;
-    }
-
-    public String getPassword(){
-        return Password;
-    }
-    public void setPassword(String Password){
-        if(Password.length() <= 8){
-            System.out.println("Password Minimal 8 Karakter");
-            System.out.print("Password : ");
-            Password = in.next();
-            setPassword(Password);
-        }else{
-            this.Password = Password;
-        }
-    }
-    
-
-    public void daftar(){
-        System.out.println("=== Daftar / Register ===");
-        System.out.print("Nama : ");
-        Mahasiswanama = in.nextLine();
-        setMahasiswanama(Mahasiswanama);
-
-        System.out.print("Nim : ");
-        Mahasiswanim = in.next();
-        setMahasiswanim(Mahasiswanim);
-
-        System.out.print("Password : ");
-        Password = in.next();
-        setPassword(Password);
-
-        
-    }
-
-    public void info(){
-        System.out.println("===================================\n");
-        System.out.println("Selamat " + getMahasiswanama() + " Anda Berhasil Mendaftar \n" );
-        System.out.println("Berikut Username Dan Password :");
-        System.out.println("\nUsername : " + getMahasiswanim());
-        System.out.println("Password : " + getPassword());
-    }
-    public static void main(String[] args) {
-        Mahasiswa mhs = new Mahasiswa();
-        mhs.daftar();
-        mhs.info();
-    }
-
+	private String nama;
+	private String nim;
+	private String password;
+	
+	Scanner keyboard = new Scanner(System.in);
+	
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNama() {return nama;}
+	
+	
+	public void setNim(String nim) {
+		this.nim = nim;
+	}
+	public String getNim() {return nim;}
+	
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword() {return password;}
+	
+	void daftar() {
+		System.out.println("===DAFTAR===");
+		System.out.print("Nama		: ");
+		nama = keyboard.nextLine();
+		
+		System.out.print("NIM		: ");
+		nim = keyboard.nextLine();
+		
+		System.out.print("Password	: ");
+		password = keyboard.nextLine();
+		
+		setNama(nama);
+		setNim(nim);
+		setPassword(password);
+		
+		System.out.println("");
+		System.out.println("Selamat " + nama + " anda berhasil mendaftar");
+		System.out.println("");
+		System.out.println("Berikut Username dan Password anda : ");
+		System.out.println("Username : " + nim);
+		System.out.print("Password : " + password);
+	}
+	
+	public static void main(String[] args) {
+		Mahasiswa mahasiswa1 = new Mahasiswa();
+		mahasiswa1.daftar();	
+	}
 }
-
-
